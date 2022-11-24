175023c84af1b464af2a3e240f0712ffdec23766
vrotex <vrotexwork@gmail.com>
2022-11-24 15:57:18 +0300

    img not done

---
+++ api.js
@@ -0,0 +1,212 @@
+require("dotenv").config();
+const { Octokit } = require('octokit')
+const expressHandleBars = require('express-handlebars');
+const linkToEngRepo = "/repos/pzn-apps/pzn-apps-content/contents/en/"
+const endOfMdFile = "?ref=main";
+const express = require('express');
+const app = express();
+
+const octokit = new Octokit({
+    auth: process.env.API_KEY,
+})
+
+const linkToEngWordDocumentAutoFill = "/repos/pzn-apps/pzn-apps-content/contents/en/word-document-auto-fill/"
+let wordAutoFillContent = [];
+
+const linkToTaskBase = "/repos/pzn-apps/pzn-apps-content/contents/en/task-base/"
+let taskBaseContent = [];
+const taskBaseFolders = [];
+
+
+// const wordAutoFillRender = []
+let wordAutoFillRouteRender = []
+
+
+// async function getIntroMd() {
+//     const introResponse = await octokit.request(`GET ${linkToEngRepo}intro.md${endOfMdFile}`, {
+
+//         owner: 'OWNER',
+//         repo: 'REPO'
+
+//     })
+
+//     addToWordArr(introResponse, introMdArray, 0)
+// }
+// getIntroMd()
+function convertObsidianLinks(text) {
+    let result = text
+
+    let links = result.match(/(?<=\[\[)(.*?)(?=\]\])/g)
+    if (links) {
+        let images = result.match(/(?<=!\[\[)(.*?)(?=\]\])/g)
+        links.map(link => {
+            if (images.find(el => { return el === link })) return
+            let parts = link.split('|')
+            if (!parts[0]) return
+            if (!parts[1]) parts[1] = parts[0].split('/').pop()
+
+            result = result.replace(
+                '[[' + link + ']]',
+                '[' + parts[1].replace(/\/.*\//g, '') + '](' + parts[0].replace(new RegExp(' ', 'g'), '').replace(/'/g, '').replace(/.*\//g, '') + ')'
+            )
+        })
+        return result
+    }
+
+}
+// let fixedText = removeLinks.replaceAll("![[pzn-apps/img", `![alt text](https://github.com/pzn-apps/pzn-apps-content/blob/main/img`)
+//     let endFixedText = fixedText.replaceAll("]]", "?raw=true)");
+const changeImgLinks = (text) => {
+    if (text) {
+        let result = text.match(/(\!\[\[pzn-apps\/img)(.*?)(\]\])/g)
+        if (result) text = text.replaceAll(/\!\[\[pzn-apps\/img/g, 'an image')
+        let straightLine = text.match(/\|\d.*\]\]/)
+
+        if (straightLine) {
+            text = text.replaceAll(straightLine, 'alt text')
+            console.log(straightLine)
+        }
+
+
+        // let result = text.replace("![[pzn-apps/img", `![alt text](https://github.com/pzn-apps/pzn-apps-content/blob/main/img`)
+        return text
+    }
+
+}
+const addToWordArr = (response, array, index) => {
+
+    let uncodedRepository = atob(response.data.content)
+    let removeLinks = convertObsidianLinks(uncodedRepository)
+    removeLinks = changeImgLinks(removeLinks)
+    // if (removeLinks) {
+    //     let addImg = changeImgLinks(removeLinks)
+    //     if (addImg) 
+    {
+        let showdown = require('showdown'),
+            converter = new showdown.Converter(),
+            convertedText = removeLinks,
+            html = converter.makeHtml(convertedText);
+
+        array[index] = html;
+        return array
+    }
+}
+
+// }
+
+
+const getContent = async (projectName, item, length, projectArray) => {
+    let arr
+    let index
+    for (let i = 0; i < 1; i++) {
+        let regExpItem = item.name.replaceAll(/ /g, "%20");
+
+        const responseData = await octokit.request(`GET ${projectName}${regExpItem}${endOfMdFile}`)
+        index = responseData.data.name[0]
+        if (projectName.includes("word-document-auto-fill/")) {
+            wordAutoFillContent = addToWordArr(responseData, projectArray, index)
+        }
+        if (projectName.includes("task-base/")) {
+            taskBaseContent = addToWordArr(responseData, projectArray, index)
+        }
+        // console.log(taskBaseContent)
+    }
+    for (let i = 0; i < length; i++) {
+
+        app.get(`/${wordAutoFillRouteRender[i]}`, (req, res) => {
+
+            res.render('layouts/main', { left_content: wordAutoFillSubFolders, right_content: wordAutoFillContent[i] })
+
+        })
+    }
+    for (let i = 0; i < length; i++) {
+        app.get(`/${taskBaseRouteRender[i]}`, (req, res) => {
+
+            res.render('layouts/main', { left_content: taskBaseSubFolders, right_content: taskBaseContent[i] })
+
+        })
+    }
+
+}
+const wordAutoFillFolders = [];
+const handlebars = expressHandleBars.create({
+    defaultLayout: 'main',
+    extname: 'hbs',
+});
+app.engine('hbs', handlebars.engine)
+
+// view engine - шаблонизатор,hbs - расширение файла
+app.set('view engine', 'hbs')
+
+app.get('/', (req, res) => {
+    res.render('layouts/main', { right_content: introMdArray })
+})
+
+
+
+app.use(express.static(__dirname + '/views/'));
+
+app.listen(5050)
+
+const dataArr = [];
+const introMdArray = [];
+
+
+
+
+async function getFoldersCount(project, foldersArray, contentArray) {
+    const response = await octokit.request(`GET ${project}`, {
+        owner: 'OWNER',
+        repo: 'REPO'
+    })
+    for (let i = 0; i < response.data.length; i++) {
+        getContent(project, response.data[i], response.data.length, contentArray)
+        foldersArray.push(response.data[i].name)
+    }
+    const newArr = foldersArray.map(item => item.replace(/ /g, ''))
+    return [newArr, foldersArray]
+}
+const wordAutoFillSubFolders = []
+getFoldersCount(linkToEngWordDocumentAutoFill, wordAutoFillFolders, wordAutoFillContent).then(item => {
+    for (let i = 0; i < item[0].length; i++) {
+        wordAutoFillRouteRender.push(item[0][i].replace(/.md/, '').replace(/'/g, ""))
+        let a = `${item[0][i].replace(/.md/, '')}`
+        let linkSubFolder = `<a class = 'left-content-subfolder' href='./${item[0][i].replace(/.md/, '').replace(/'/g, '')}'>${item[1][i].replace(/.md/, '')}</a>`
+        wordAutoFillSubFolders.push(linkSubFolder)
+        // wordAutoFillRender.push(a)
+    }
+})
+let taskBaseRouteRender = []
+const taskBaseSubFolders = []
+getFoldersCount(linkToTaskBase, taskBaseFolders, taskBaseContent).then(item => {
+    for (let i = 0; i < item[0].length; i++) {
+        taskBaseRouteRender.push(item[0][i].replace(/.md/, '').replace(/'/g, ""))
+        // let a = `./${taskBaseFolders[i].replace(/.md/, '').replace(/'/g, '')}`
+        // taskBaseRouteRender.push(a)
+        let linkSubFolder = `<a class = 'left-content-subfolder' href='./${item[0][i].replace(/.md/, '').replace(/'/g, '')}'>${item[1][i].replace(/.md/, '')}</a>`
+        taskBaseSubFolders.push(linkSubFolder)
+
+    }
+    app.get('/', (req, res) => {
+        res.render("main.hbs",
+            { left_content: taskBaseRouteRender.map(item => item) }
+        )
+    })
+
+})
+
+
+const pathCreate = (foldersArray) => {
+    foldersArray = foldersArray.forEach(item => item.replace(/ /g, ""));
+    return foldersArray
+}
+pathCreate(wordAutoFillFolders)
+
+
+
+
+
+
+
+
+
---
+++ views/octokitEng.js
@@ -0,0 +1,152 @@
+import { Octokit, App } from "https://cdn.skypack.dev/octokit?dts";
+import API_KEY from "./config.js";
+const api_key = API_KEY;
+const mainDescription = document.getElementById('main-description');
+const leftContent = document.querySelector(".left-content");
+const wordAutoFillFolders = [];
+const wordAutoFillContent = [];
+const projectHeader = Array.from(document.querySelectorAll('.project-header'));
+const linkToEngRepo = "/repos/pzn-apps/pzn-apps-content/contents/en/"
+const linkToEngWordDocumentAutoFill = "/repos/pzn-apps/pzn-apps-content/contents/en/word-document-auto-fill/"
+const endOfMdFile = "?ref=main";
+const octokit = new Octokit({
+    auth: api_key,
+})
+
+
+
+const linkToTaskBase = "/repos/pzn-apps/pzn-apps-content/contents/en/task-base/"
+const taskBaseContent = [];
+const taskBaseFolders = [];
+
+//get intro md file eng
+const introMdArray = [];
+// async function getIntroMd() {
+//     const introResponse = await octokit.request(`GET ${linkToEngRepo}intro.md${endOfMdFile}`, {
+
+//         owner: 'OWNER',
+//         repo: 'REPO'
+
+//     })
+
+//     addToWordArr(introResponse, introMdArray)
+//     mainDescription.innerHTML = introMdArray[0];
+// }
+// getIntroMd()
+const addToWordArr = (response, array) => {
+    //[Link text Here](https://link-url-here.org)
+    let uncodedRepository = atob(response.data.content)
+    //change link
+    let fixedText = uncodedRepository.replaceAll("![[pzn-apps/img", "![alt text](https://github.com/pzn-apps/pzn-apps.github.io/blob/main/img")
+    let endFixedText = fixedText.replaceAll("]]", "?raw=true)");
+    var converter = new showdown.Converter(),
+        convertedText = endFixedText,
+        html = converter.makeHtml(convertedText);
+
+    array.push(html);
+    return array
+}
+const enProjects = [];
+const getEnProjects = async () => {
+    const response = await octokit.request(`GET ${linkToEngRepo}`, {
+        owner: 'OWNER',
+        repo: 'REPO'
+    })
+    for (let i = 0; i < response.data.length; i++) {
+        if (!response.data[i].name.includes(".md")) {
+            enProjects.push(response.data[i].name)
+        }
+    }
+}
+getEnProjects()
+async function getFoldersCount(project, foldersArray, contentArray) {
+    const response = await octokit.request(`GET ${project}`, {
+        owner: 'OWNER',
+        repo: 'REPO'
+    })
+    for (let i = 0; i < response.data.length; i++) {
+        getContent(project, response.data[i], response.data.length, contentArray)
+        foldersArray.push(response.data[i].name)
+    }
+    console.log(contentArray)
+    return foldersArray
+}
+getFoldersCount(linkToEngWordDocumentAutoFill, wordAutoFillFolders, wordAutoFillContent)
+getFoldersCount(linkToTaskBase, taskBaseFolders, taskBaseContent)
+
+const getContent = async (projectName, item, length, projectArray) => {
+    let arr
+    for (let i = 0; i < 1; i++) {
+        let regExpItem = item.name.replaceAll(/ /g, "%20");
+
+        const responseData = await octokit.request(`GET ${projectName}${regExpItem}${endOfMdFile}`)
+
+        arr = addToWordArr(responseData, projectArray)
+    }
+
+}
+
+
+// переход без подгрузки
+
+// const href = Array.from(document.querySelectorAll('.hrefTo'))
+// href.map(item => item.addEventListener('click', (e) => {
+//     let path = e.target.innerText.replace(/\s/g, '')
+//     window.history.pushState({ hrefTo: path }, 'lorem', path)
+//     window.addEventListener('popstate', (e) => {
+//         e.state ? console.log(e.state) : console.error(e.state)
+//     })
+// }))
+
+
+// const hrefFolder = Array.from(document.querySelectorAll('.hrefTo'))
+// hrefFolder.map(item => item.addEventListener('click', (e) => {
+//     let path = e.target.href.replace(/\s/g, '')
+//     let newPath = path.replace(/\'/g, '')
+//     console.log(newPath)
+//     window.history.pushState({ hrefTo: newPath }, 'lorem', newPath)
+
+//     window.addEventListener('popstate', (e) => {
+//         e.state ? console.log(e.state) : console.error(e.state)
+//     })
+// }))
+
+
+
+let pathWordDocument = []
+const createSubFolder = (arr, index, subacontent) => {
+
+    document.querySelector(".left-content").innerHTML = '';
+    mainDescription.innerHTML = '';
+    pathWordDocument = arr.map(item => item.replace(/ /g, '').replace(/.md/g, '').replace(/'/g, ''));
+    if (leftContent.children.length === 0) {
+        for (let i = 0; i < arr.length; i++) {
+            let a = document.createElement('a')
+            a.href = pathWordDocument[i];
+            a.classList.add('left-content-subfolder');
+            a.innerHTML = arr[i].replace(/.md/g, '')
+            document.querySelector(".left-content").append(a)
+            a.addEventListener('click', () => {
+                mainDescription.innerHTML = subacontent[i];
+            })
+        }
+        return pathWordDocument
+    }
+
+}
+let resp = 0;
+
+projectHeader.map((item, index) => item.addEventListener('click', () => {
+    projectHeader.map(item => {
+        item.style.backgroundColor = 'rgb(207, 207, 207)';
+        item.style.transform = "scale(1)"
+    })
+    item.style.backgroundColor = "white"
+    item.style.transform = "scale(1.09)"
+    if (index === 0) createSubFolder(wordAutoFillFolders, index, wordAutoFillContent)
+    if (index === 1) createSubFolder(taskBaseFolders, index, taskBaseContent)
+    // if (index === 2) createSubFolder(intelligentInfoArrs, index)
+    // if (index === 3) createSubFolder(bridgeArrs, index)
+
+}))
+
